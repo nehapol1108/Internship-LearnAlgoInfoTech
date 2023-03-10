2b636a7c4f02a4771f67edc11ea02039e3e94c5b
nehapol <nehapol40@gmail.com>
2023-03-11 00:29:27 +0530

    deployment 4

---
+++ backend2/models/TransferSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+let TransactionSchema = new mongoose.Schema({
+  from: {
+    type:mongoose.Types.ObjectId,
+    required:true,
+    ref:'CUSTOMERS'
+  },
+  to: {
+    type:mongoose.Types.ObjectId,
+    required:true,
+    ref:'CUSTOMERS'
+  },
+  amount: {
+    type:Number,
+    required:true,
+  },
+  statement:{
+    type:String,
+    default:""
+  }
+  },{timestamps:true});
+
+const Transaction = mongoose.model("Transaction", TransactionSchema);
+
+module.exports = Transaction;
